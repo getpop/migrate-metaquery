ca496901915bf0c4aba0671dec1c32f19067ea6d
Leo <losoviz@gmail.com>
2019-07-22 12:17:47 +0530

    Extend component to initialize plugin

--- initialize.php
+++ initialize.php
@@ -1,2 +1,17 @@
-<?php 
-require_once 'migrate/pop-metaquery.php';
+<?php
+$class = (new class() extends \PoP\Root\Component\AbstractComponent
+{
+    /**
+     * Boot component
+     *
+     * @return void
+     */
+    public static function boot()
+    {
+        parent::boot();
+
+        // Initialize code
+        require_once 'migrate/pop-metaquery.php';  
+    }
+});
+$class::init();
